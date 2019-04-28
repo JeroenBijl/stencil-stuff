1552114cd34904683bd400f7e448a5a9084a0cda
Desiree Hof <desiree.hof@rabobank.nl>
2019-04-28 14:21:59 +0200

    created component library of project instead of app

--- stencil.config.ts
+++ stencil.config.ts
@@ -3,15 +3,14 @@ import { sass } from '@stencil/sass';
 
 export const config: Config = {
   namespace: 'cw-components',
+  srcDir: './src',
+  outputTargets: [
+    {
+      dir: 'dist',
+      type: 'dist',
+    }
+  ],
   plugins: [
     sass()
   ],
-  outputTargets:[
-    { type: 'dist' },
-    { type: 'docs' },
-    {
-      type: 'www',
-      serviceWorker: null // disable service workers
-    }
-  ]
 };
